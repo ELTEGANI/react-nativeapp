e7cdddacc7359e073709607f89f62bcc62f12545
ELTEGANI <tiganimoh@gmail.com>
2024-03-23 15:05:23 +0400

    add basic layout

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import {useState} from 'react'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native';
 
-import styles from './welcome.style'
+import { useRouter } from 'expo-router';
+import styles from './welcome.style';
+import { icons,SIZES } from '../../../constants';
+
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+       <Text styles={styles.userName}>Hello Adrian</Text>
+       <Text styles={styles.WelcomeMessage}>Find Your Perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=''
+            onChange={()=>{}}
+            placeholder='What are you loooking'
+          />
+        </View>
+      </View>
     </View>
   )
 }
